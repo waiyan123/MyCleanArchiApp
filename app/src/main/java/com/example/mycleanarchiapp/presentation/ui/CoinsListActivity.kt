b866e4c408fb9c078ec1daa397839820f4405425
waiyan123 <itachialpha@gmail.com>
2022-09-07 00:41:16 +0630

    initial commit of state flow and shared flow

--- app/src/main/java/com/example/mycleanarchiapp/presentation/ui/CoinsListActivity.kt
+++ app/src/main/java/com/example/mycleanarchiapp/presentation/ui/CoinsListActivity.kt
@@ -3,12 +3,18 @@ package com.example.mycleanarchiapp.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.lifecycleScope
 import com.example.mycleanarchiapp.R
 import com.example.mycleanarchiapp.common.Resource
 import com.example.mycleanarchiapp.presentation.viewmodels.CoinsListViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CoinsListActivity : AppCompatActivity() {
@@ -19,17 +25,26 @@ class CoinsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_list)
 
-        mViewModel.coinsListStateLivedata.observe(this) {result->
-            when(result) {
-                is Resource.Success -> {
-                    Log.d("test---","coins size ${result.data?.size}")
+        lifecycleScope.launch {
+            mViewModel.coinListState.collect {result->
+                when(result) {
+                    is Resource.Success -> {
+                        Log.d("test---","coins size ${result.data?.size}")
+                    }
+                    is Resource.Error -> {
+                        Log.d("test---","${result.message}")
+                    }
+                    is Resource.Loading -> {
+                        Log.d("test---","Loading")
+                    }
                 }
-                is Resource.Error -> {
-                    Log.d("test---","${result.message}")
-                }
-                is Resource.Loading -> {
-                    Log.d("test---","Loading")
-                }
+            }
+        }
+
+        lifecycleScope.launch{
+            mViewModel.toastCompleteCoinList.collectLatest {
+//                Toast.makeText(CoinsListActivity,it,Toast.LENGTH_SHORT).show()
+                Log.d("test---","$it")
             }
         }
     }
--- app/src/main/java/com/example/mycleanarchiapp/presentation/viewmodels/CoinsListViewModel.kt
+++ app/src/main/java/com/example/mycleanarchiapp/presentation/viewmodels/CoinsListViewModel.kt
@@ -6,60 +6,55 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mycleanarchiapp.common.Resource
 import com.example.mycleanarchiapp.domain.model.Coin
 import com.example.mycleanarchiapp.domain.use_case.GetCoinsUseCase
 import com.example.mycleanarchiapp.presentation.coin_list.CoinListState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CoinsListViewModel @Inject constructor(private val getCoinsUseCase: GetCoinsUseCase) : ViewModel(){
 
-    private val _state = mutableStateOf<CoinListState>(CoinListState())
-    val coinListState : State<CoinListState>  = _state
+    private val _CoinListState = MutableStateFlow<Resource<List<Coin>>?>(null)
+    val coinListState = _CoinListState.asStateFlow()
 
-    val coinsListStateLivedata = MutableLiveData<Resource<List<Coin>>>()
+    private val _toastCompleteCoinList = MutableSharedFlow<String>()
+    val toastCompleteCoinList = _toastCompleteCoinList.asSharedFlow()
 
     init {
         getCoins()
+
+
     }
 
     private fun getCoins() {
         val coins = getCoinsUseCase()
-        GlobalScope.launch {
+        viewModelScope.launch {
             coins.collect{result->
                 when(result) {
                     is Resource.Success -> {
-                        coinsListStateLivedata.postValue(Resource.Success(result.data ?: emptyList()))
-                        _state.value = CoinListState(coins = result.data ?: emptyList())
+                        _CoinListState.value = Resource.Success(result.data ?: emptyList())
+                        completeCoinList()
                     }
                     is Resource.Error -> {
-                        coinsListStateLivedata.postValue(Resource.Error(result.message ?: "An unexpected error occurred"))
-                        _state.value = CoinListState(error = result.message ?: "An unexpected error occurred")
+                        _CoinListState.value = Resource.Error(result.message ?: "An unexpected error occurred")
                     }
                     is Resource.Loading -> {
-                        coinsListStateLivedata.postValue(Resource.Loading())
-                        _state.value = CoinListState(isLoading = true)
+                        _CoinListState.value = Resource.Loading()
                     }
                 }
             }
         }
-//        coins.onEach {result ->
-//            when(result) {
-//                is Resource.Success -> {
-//                    _state.value = CoinListState(coins = result.data ?: emptyList())
-//                }
-//                is Resource.Error -> {
-//                    _state.value = CoinListState(error = result.message ?: "An unexpected error occurred")
-//                }
-//                is Resource.Loading -> {
-//                    _state.value = CoinListState(isLoading = true)
-//                }
-//            }
-//        }
+    }
+
+    private fun completeCoinList() {
+        viewModelScope.launch {
+            _toastCompleteCoinList.emit("Complete updating coin list!")
+        }
     }
 }
