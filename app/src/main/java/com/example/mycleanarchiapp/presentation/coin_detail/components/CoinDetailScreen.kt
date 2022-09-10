995e51991f0f8a1e4976fd852e82d23fb26428a9
waiyan123 <itachialpha@gmail.com>
2022-09-10 13:07:15 +0630

    changes

--- app/src/main/java/com/example/mycleanarchiapp/presentation/coin_detail/components/CoinDetailScreen.kt
+++ app/src/main/java/com/example/mycleanarchiapp/presentation/coin_detail/components/CoinDetailScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,9 +37,18 @@ fun CoinDetailScreen(
     }
 }
 
-
 @Composable
 @Preview(showBackground = true)
+fun samplePreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        FirstRow()
+        SecondRow()
+        ThirdRow()
+    }
+}
+
+
+@Composable
 fun FirstRow(
     rank: String = "1",
     name: String = "Bitcoin",
@@ -48,21 +58,59 @@ fun FirstRow(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp),
+            .padding(10.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
             text = "$rank. $name ($symbol)",
             style = MaterialTheme.typography.h5,
-            overflow = TextOverflow.Ellipsis
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(8f)
         )
         Text(
             text = active,
             textAlign = TextAlign.End,
             color = MaterialTheme.colors.primary,
             modifier = Modifier.align(CenterVertically)
-
-
+                .weight(2f),
+            fontStyle = FontStyle.Italic
         )
+    }
+}
+
+@Composable
+fun SecondRow(
+    paragraph: String = "Enabling this setting will require contributors to sign off on commits made through GitHub’s web interface. Signing off is a way for contributors to affirm that their commit complies with the repository's terms, commonly the Developer Certificate of Origin (DCO). Learn more about signing off on commits."
+) {
+    Row(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = paragraph,
+            textAlign = TextAlign.Left,
+            style = MaterialTheme.typography.body2
+        )
+    }
+}
+
+@Composable
+fun ThirdRow() {
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .padding(10.dp)) {
+        Text(
+            text = "Tags",
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Left
+        )
+    }
+}
+
+@Composable
+fun FourthRow() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+
     }
 }
